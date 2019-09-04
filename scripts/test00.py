627fe60bb8168d5cb051b4253e7b3ee138dd69d1
tian <949716705@qq.com>
2019-09-04 22:51:56 +0800

    ww

--- scripts/test00.py
+++ scripts/test00.py
@@ -1,8 +1,9 @@
-import pytest,allure
+import pytest, allure
 
 
 class Test:
-
+    allure.description("这是一个测试")
+    pytest.mark.usefixtures()
     def test_001(self):
         print("---------------1")
         assert True
